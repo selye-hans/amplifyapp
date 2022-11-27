ee3a8649be8057e482630cc13ee5c3aefccd366b
EC2 Default User <ec2-user@ip-172-31-37-73.ap-northeast-1.compute.internal>
2022-11-27 14:53:50 +0000

    add authenticaton

--- src/App.js
+++ src/App.js
@@ -2,26 +2,37 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+import { Amplify } from 'aws-amplify';
+import { Authenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+
+Amplify.configure({
+  aws_cognito_region: 'ap-northeast-1',
+  aws_user_pools_id: 'ap-northeast-1_in3GU2QKT',
+  aws_user_pools_web_cliend_id: '33rn2g1i0ejso704cihqc3pl3s',
+});
+
+export function SignIn() {
+  return (
+    <Authenticator>
+      {({ signOut, user }) => (
+        <main>
+          <h2>Hello {user.username}</h2>
+          <button onClick={signOut}>Sign Out</button>
+        </main>
+      )}
+    </Authenticator>
+  );
+}
+
+export function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello from V2</h1>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Hello from V3</h1>
+        <SignIn />
       </header>
     </div>
   );
 }
-
-export default App;
